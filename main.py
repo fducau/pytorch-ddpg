0f8aaf511f62d9dbc26e12fa356fc9ddf762f95d
fducau <fnd212@nyu.edu>
2018-04-24 19:00:55 -0400

    Format changes, no functionality changed.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+# !/usr/bin/env python3 
 
 import numpy as np
 import argparse
@@ -13,12 +13,23 @@ from util import *
 
 gym.undo_logger_setup()
 
-def train(num_iterations, gent, env,  evaluate, validate_steps, output, max_episode_length=None, debug=False):
+
+def train(
+    num_iterations,
+    agent,
+    env,
+    evaluate,
+    validate_steps,
+    output,
+    max_episode_length=None,
+    debug=False
+):
 
     agent.is_training = True
     step = episode = episode_steps = 0
     episode_reward = 0.
     observation = None
+
     while step < num_iterations:
         # reset if it is the start of episode
         if observation is None:
@@ -27,39 +38,48 @@ def train(num_iterations, gent, env,  evaluate, validate_steps, output, max_epis
 
         # agent pick action ...
         if step <= args.warmup:
+            # Gets action and stores it in the agent
             action = agent.random_action()
         else:
             action = agent.select_action(observation)
-        
+
         # env response with next_observation, reward, terminate_info
         observation2, reward, done, info = env.step(action)
         observation2 = deepcopy(observation2)
-        if max_episode_length and episode_steps >= max_episode_length -1:
+
+        if max_episode_length and episode_steps >= max_episode_length - 1:
             done = True
 
-        # agent observe and update policy
+        # agent observe and update policy (save in experience memory)
         agent.observe(reward, observation2, done)
-        if step > args.warmup :
+        if step > args.warmup:
             agent.update_policy()
-        
+
         # [optional] evaluate
         if evaluate is not None and validate_steps > 0 and step % validate_steps == 0:
-            policy = lambda x: agent.select_action(x, decay_epsilon=False)
+
+            def policy(x):
+                return agent.select_action(x, decay_epsilon=False)
+
             validate_reward = evaluate(env, policy, debug=False, visualize=False)
-            if debug: prYellow('[Evaluate] Step_{:07d}: mean_reward:{}'.format(step, validate_reward))
+            if debug:
+                prYellow('[Evaluate] Step_{:07d}: mean_reward:{}'.format(step, validate_reward))
 
         # [optional] save intermideate model
-        if step % int(num_iterations/3) == 0:
+        if step % int(num_iterations / 3) == 0:
             agent.save_model(output)
 
-        # update 
+        # update
         step += 1
         episode_steps += 1
         episode_reward += reward
         observation = deepcopy(observation2)
 
-        if done: # end of episode
-            if debug: prGreen('#{}: episode_reward:{} steps:{}'.format(episode,episode_reward,step))
+        if done:    # end of episode
+            if debug:
+                prGreen('#{}: episode_reward:{} steps:{}'.format(episode,
+                                                                 episode_reward,
+                                                                 step))
 
             agent.memory.append(
                 observation,
@@ -72,6 +92,7 @@ def train(num_iterations, gent, env,  evaluate, validate_steps, output, max_epis
             episode_steps = 0
             episode_reward = 0.
             episode += 1
+
 
 def test(num_episodes, agent, env, evaluate, model_path, visualize=True, debug=False):
 
@@ -102,14 +123,14 @@ if __name__ == "__main__":
     parser.add_argument('--window_length', default=1, type=int, help='')
     parser.add_argument('--tau', default=0.001, type=float, help='moving average for target network')
     parser.add_argument('--ou_theta', default=0.15, type=float, help='noise theta')
-    parser.add_argument('--ou_sigma', default=0.2, type=float, help='noise sigma') 
-    parser.add_argument('--ou_mu', default=0.0, type=float, help='noise mu') 
+    parser.add_argument('--ou_sigma', default=0.2, type=float, help='noise sigma')
+    parser.add_argument('--ou_mu', default=0.0, type=float, help='noise mu')
     parser.add_argument('--validate_episodes', default=20, type=int, help='how many episode to perform during validate experiment')
     parser.add_argument('--max_episode_length', default=500, type=int, help='')
     parser.add_argument('--validate_steps', default=2000, type=int, help='how many steps to perform a validate experiment')
     parser.add_argument('--output', default='output', type=str, help='')
     parser.add_argument('--debug', dest='debug', action='store_true')
-    parser.add_argument('--init_w', default=0.003, type=float, help='') 
+    parser.add_argument('--init_w', default=0.003, type=float, help='')
     parser.add_argument('--train_iter', default=200000, type=int, help='train iters each timestep')
     parser.add_argument('--epsilon', default=50000, type=int, help='linear decay of exploration policy')
     parser.add_argument('--seed', default=-1, type=int, help='')
@@ -131,18 +152,21 @@ if __name__ == "__main__":
     nb_states = env.observation_space.shape[0]
     nb_actions = env.action_space.shape[0]
 
-
     agent = DDPG(nb_states, nb_actions, args)
-    evaluate = Evaluator(args.validate_episodes, 
-        args.validate_steps, args.output, max_episode_length=args.max_episode_length)
+    evaluate = Evaluator(args.validate_episodes,
+                         args.validate_steps,
+                         args.output,
+                         max_episode_length=args.max_episode_length)
 
     if args.mode == 'train':
-        train(args.train_iter, agent, env, evaluate, 
-            args.validate_steps, args.output, max_episode_length=args.max_episode_length, debug=args.debug)
+        train(args.train_iter, agent, env, evaluate,
+              args.validate_steps, args.output,
+              max_episode_length=args.max_episode_length,
+              debug=args.debug)
 
     elif args.mode == 'test':
         test(args.validate_episodes, agent, env, evaluate, args.resume,
-            visualize=True, debug=args.debug)
+             visualize=True, debug=args.debug)
 
     else:
         raise RuntimeError('undefined mode {}'.format(args.mode))
